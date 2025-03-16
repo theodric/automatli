7b3a4fb74d693742c26a1be119ff1a3f1cf39345
þēodrīċ <theodric@users.noreply.github.com>
2025-03-16 20:57:23 +0000

    switched to asyncio

--- automatli.py
+++ automatli.py
@@ -1,113 +1,66 @@
 #!/usr/bin/env python3
-# automatli2024 post-Elon prototyp 2.1
+# Post-El0n proto2 - still using twikit
+# I made ChatGPT refactor it to implement asyncio (new hotness), and it worked! 
+# What a world we live in.
 
-import sys
-import time
+import asyncio
 import datetime
 import logging
 import random
-from pathlib import Path
+import os
 from twikit import Client
 
-## twikit login setup:
-client = Client('en-US')
-
-# Enter your account information here
-# Uncomment and use this for the first login
-USERNAME = "INPUT YOUR USERNAME HERE"
-EMAIL = "ACCOUNTEMAIL@PROVIDER.TLD"
-PASSWORD = "YOUR-P4SSW0RD"
-#
-client.login(
-    auth_info_1=USERNAME,
-    auth_info_2=EMAIL,
-    password=PASSWORD
-)
-client.save_cookies('cookies.json')
-
-# For the second+ login, comment out lines 16-25 inclusive and uncomment the line below:
-# client.load_cookies('cookies.json')
-
-ICraw = './IC.txt'
-IPraw = './IP.txt'
-SPraw = './SP.txt'
-TPraw = './TP.txt'
-FDraw = './FD.txt'
-blank = ""
-
-ICFile = open(ICraw, 'r')
-IC = ICFile.readlines()
-ICFile.close()
-
-IPFile = open(IPraw, 'r')
-IP = IPFile.readlines()
-IPFile.close()
-
-SPFile = open(SPraw, 'r')
-SP = SPFile.readlines()
-SPFile.close()
-
-TPFile = open(TPraw, 'r')
-TP = TPFile.readlines()
-TPFile.close()
-
-FDFile = open(FDraw, 'r')
-FD = FDFile.readlines()
-FDFile.close()
-## Configure logging
+# configure logging
 log_format = '%(asctime)s - %(levelname)s - %(message)s'
 logging.basicConfig(filename='/var/log/automatli.log', level=logging.INFO, format=log_format)
 
-## Function to print the time of the next tweet
+# account info
+USERNAME = "INPUT YOUR USERNAME HERE"
+EMAIL = "ACCOUNTEMAIL@PROVIDER.TLD"
+PASSWORD = "YOUR-P4SSW0RD"
+
+client = Client('en-US')
+
+async def login():
+    if os.path.exists('cookies.json'):
+        client.load_cookies('cookies.json')
+    else:
+        await client.login(auth_info_1=USERNAME, auth_info_2=EMAIL, password=PASSWORD)
+        client.save_cookies('cookies.json')
+
+async def load_data(file_path):
+    return await asyncio.to_thread(lambda: open(file_path, 'r').readlines())
+
+async def generate_tweet(IC, IP, SP, TP, FD):
+    ICinter = random.choice(IC) if random.randrange(1, 6) < 5 else ""
+    FDinter = random.choice(FD) if random.randrange(1, 6) <= 4 else ""
+    return str.lower(ICinter.strip()) + str.lower(random.choice(IP).strip()) + \
+           str.lower(random.choice(SP).strip()) + str.lower(random.choice(TP).strip()) + \
+           str.lower(FDinter.strip())
+
 def calculate_next_tweet_time(minutes_until_next_tweet):
-	current_time = datetime.datetime.now()
-	next_tweet_time = current_time + datetime.timedelta(minutes=minutes_until_next_tweet)
-	return next_tweet_time.strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.datetime.now()
+    next_tweet_time = current_time + datetime.timedelta(minutes=minutes_until_next_tweet)
+    return next_tweet_time.strftime("%Y-%m-%d %H:%M:%S")
 
-while True:
-	ICidx = random.randrange(len(IC))
-	IPidx = random.randrange(len(IP))
-	SPidx = random.randrange(len(SP))
-	TPidx = random.randrange(len(TP))
-	FDidx = random.randrange(len(FD))
+async def main():
+    await login()
+    
+    IC, IP, SP, TP, FD = await asyncio.gather(
+        load_data('./IC.txt'), load_data('./IP.txt'), load_data('./SP.txt'), 
+        load_data('./TP.txt'), load_data('./FD.txt')
+    )
+    
+    while True:
+        tweet = await generate_tweet(IC, IP, SP, TP, FD)
+        print(f"Tweeting: {tweet}")
+        logging.info(f"Tweeting: {tweet}")
+        await client.create_tweet(tweet)
 
-	ICinter = IC[ICidx]
-	ICrand = random.randrange(1,6)
-	if ICrand < 5:
-		ICinter = ""
+        tweet_frequency = random.randint(1620, 13370)
+        print(f"Next tweet in ~{round(tweet_frequency / 60)} minutes, at {calculate_next_tweet_time(round(tweet_frequency / 60))}")
+        logging.info(f"Next tweet in ~{round(tweet_frequency / 60)} minutes, at {calculate_next_tweet_time(round(tweet_frequency / 60))}")
 
-	FDinter = FD[FDidx]
-	FDrand = random.randrange(1,6)
-	if FDrand > 4:
-		FDinter = ""
+        await asyncio.sleep(tweet_frequency)
 
-	tvit = str.lower(ICinter.rstrip("\r\n")) + str.lower(IP[IPidx].rstrip("\r\n")) + str.lower(SP[SPidx].rstrip("\r\n")) + str.lower(TP[TPidx].rstrip("\r\n")) + str.lower(FDinter.rstrip("\r\n"))
-	print("Tweeting the following nonsense: " + tvit)
-	logging.info("Tweeting the following nonsense: " + tvit)
-
-	client.create_tweet(tvit)
-
-	## You have two options for the below.
-	## You should ONLY uncomment one of these!
-
-	## Option 1: hardcode the frequency of tweets posted, in SECONDS
-	## Don't set this too low, or you may get rate-limited, or even put in Twitter Jail!
-	## Reminder: there are 3600 seconds in an hour, and 86400 seconds in a day.
-#	tweetFrequency = 14400
-
-	## Option 2: 
-	## Have a timer which fires randomly within a range of intervals in SECONDS.
-	## I recommend this, since it looks more like how a human would tweet, rather than
-	## being on rails like a regular timer.
-	tweetFrequency = random.randint(1620, 13370)
-	
-	print("Next tweet in approximately", round(tweetFrequency / 60), "minutes, at", calculate_next_tweet_time(round(tweetFrequency / 60)))
-	logging.info("Next tweet in approximately %s minutes, at %s", round(tweetFrequency / 60), calculate_next_tweet_time(round(tweetFrequency / 60)))	
-
-# I found the countdown in stdout annoying, so I replaced this function with the above, but it's still here if you want it.
-#	for i in range(tweetFrequency, 0, -1):
-#		time.sleep(1)
-#		sys.stdout.write(str((i - 1))+' ')
-#		sys.stdout.flush()
-
-	time.sleep(tweetFrequency)
+asyncio.run(main())
